c11afbc789f1afbb248c656180098a61258b7242
shohaib <shohaib@SHOHAIB2A69>
2017-10-18 12:40:05 +0200

    - La til lagdeling - Log in - Seeding for admin

---
+++ Model/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperWing.Model
+{
+    public class Admin
+    {
+        [Required(ErrorMessage = "Navn må oppgis")]
+        public String Navn { get; set; }
+        [Required(ErrorMessage = "Passord må oppgis")]
+        public String Passord { get; set; }
+    }
+}
