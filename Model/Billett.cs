cf095dc39a0fd91cd194db3e6c85ace8a0cfe048
pontusskold <pontusskold@PONTUSSKLD1C1D>
2017-10-20 08:33:32 +0200

    Flyttat klasser till DAL och Model

---
+++ Model/Billett.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HyperWing.Model
+{
+    public class Billett
+    {
+        [Key]
+        public int Id { get; set; }
+        public String PersonNr { get; set; }
+        public String Navn { get; set; }
+        public String ByFra { get; set; }
+        public String Mellomlanding { get; set; }
+        public String ByTil { get; set; }
+        public String Epost { get; set; }
+        public int Telefon { get; set; }
+        public DateTime Avgang { get; set; }
+        public DateTime Ankomst { get; set; }
+        public String Flyplass { get; set; }
+        public double Pris { get; set; }
+    } 
+}
