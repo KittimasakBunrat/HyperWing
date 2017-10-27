0b7eeb85e749fdd2661c76ea7407c3d8d34a9c71
shohaib <shohaib@SHOHAIB2A69>
2017-10-27 15:54:26 +0200

    - La til logging

--- DAL/DB.cs
+++ DAL/DB.cs
@@ -9,54 +9,6 @@ namespace HyperWing.DAL
 {
     public class DB
     {
-        public bool adminDB(Admin admin)
-        {
-            using (var db = new FlyContext())
-            {
-                dbAdmin funnetAdmin = db.Administratorer.FirstOrDefault(b => b.Navn == admin.Navn);
-                if (funnetAdmin != null)
-                {
-                    byte[] testPass = lagHash(admin.Passord + funnetAdmin.Salt);
-                    bool riktigBruker = funnetAdmin.Passord.SequenceEqual(testPass);
-                    return riktigBruker;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-
-        public byte[] lagHash(String hash)
-        {
-            byte[] innData, utData;
-            var algoritme = SHA256.Create();
-            innData = Encoding.UTF8.GetBytes(hash);
-            utData = algoritme.ComputeHash(innData);
-            return utData;
-        }
-
-        public string lagSalt()
-        {
-            byte[] randomArray = new byte[10];
-            string randomString;
-
-            var rng = new RNGCryptoServiceProvider();
-            rng.GetBytes(randomArray);
-            randomString = Convert.ToBase64String(randomArray);
-            return randomString;
-        }
-
-        public string HashStreng(String innStreng)
-        {
-            byte[] hash = lagHash(innStreng);
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                stringBuilder.Append(hash[i].ToString("x2"));
-            }
-            return stringBuilder.ToString();
-        }
 
         //Från gammal DB
         public List<FlyReise> listAlleReiser()
@@ -201,15 +153,11 @@ namespace HyperWing.DAL
 
             using (var db = new FlyContext())
             {
-                try
-                {
+
                     db.Kunder.Add(nyKunde);
                     db.SaveChanges();
-                }
-                catch (Exception ex)
-                {
-                    Console.Write("Feil ved innsetting av kunde");
-                }
+
+
             }
 
             return nyKunde;
@@ -291,7 +239,11 @@ namespace HyperWing.DAL
             return kundeListe;
         }
 
-
+        public bool adminDB(Admin admin)
+        {
+            var db = new DB();
+            return db.adminDB(admin);
+        }
     }
 }
 
